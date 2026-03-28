12ffb78b5e3ffd47feb42fbe30ae24197a3e0397
mmohan0986-stack <mmohan0986@gmail.com>
2026-03-29 00:50:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker Installed');
-});
+function updateScore(id) {
+
+  let scores = [
+    "RCB: 140/4 (17 overs)",
+    "RCB: 155/5 (18 overs)",
+    "RCB: 170/6 (19 overs)",
+    "RCB: 185/7 (20 overs)"
+  ];
+
+  let random = Math.floor(Math.random() * scores.length);
+
+  document.getElementById(id).innerText = scores[random];
+}
